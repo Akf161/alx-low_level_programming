2252d6f2792a2e8bd435f3768fe1f7673e8fead3
Akf161 <akolj161@gmail.com>
2022-12-09 07:04:13 -0800

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i != j && j != k && i != k && i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
