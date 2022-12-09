0ac6c75d72deaa3123d28d955f863c29ce1f086f
Akf161 <akolj161@gmail.com>
2022-12-09 07:51:11 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,15 @@ int main(void)
 	int sumij;
 	int sumxy;
 
-	for (i = 0 && x = 0; i < 10 && x < 10; i++ && x++)
+	x = 0;
+	y = 0;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0 && y = 0; j < 10 && y < 10; j++ && y++)
+		x++;
+		for (j = 0; j < 10; j++)
 		{
+			y++;
 			if (sumij < sumxy)
 			{
 				putchar(i + '0');
