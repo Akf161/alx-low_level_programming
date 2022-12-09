a761ef225be7b25da85151f45b41b4ce34a26453
Akf161 <akolj161@gmail.com>
2022-12-09 06:49:52 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (i != j)
+			if (i != j && i < j)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
