4ebfb6216f45e46c27f7b096caaf4813cc591ff0
Akf161 <akolj161@gmail.com>
2022-12-14 02:12:48 -0800

    main.h and 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests the postitive_or_negative() function at zero
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+
+	return (0);
+}
