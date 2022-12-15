75eb295c1043fb4260a522018c958dc033c55942
Akf161 <akolj161@gmail.com>
2022-12-15 06:51:51 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0 - 9 except 2 and 4
+ * print_most_numbers - prints 0 - 9 except 2 and 4
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	char i;
 
