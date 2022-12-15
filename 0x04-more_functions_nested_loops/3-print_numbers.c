84baefd323307fab0b366c34f7221379f80c6a76
Akf161 <akolj161@gmail.com>
2022-12-15 06:32:59 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
