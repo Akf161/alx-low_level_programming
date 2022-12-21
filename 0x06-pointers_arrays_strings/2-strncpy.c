7ae1dd08e482c7210a1bde68c949b60f794ebc5b
Akf161 <akolj161@gmail.com>
2022-12-21 08:00:29 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: the buffer storing the string copy
  * @src: the source string
  * @n: the maximum number of bytes to be copied
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
