256e2308955c37fc9611c5d4e211349ee564d5e1
Akf161 <akolj161@gmail.com>
2022-12-22 07:59:32 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -18,10 +18,10 @@ void print_number(int n)
 		m = -n;
 	}
 
-	if ((n / 10) > 0)
+	if ((m / 10) > 0)
 	{
-		print_number(num / 10);
+		print_number(m / 10);
 	}
 
-	_putchar((num % 10) + '0');
+	_putchar((m % 10) + '0');
 }
