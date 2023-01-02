435598b02d01e354f182e44e9bf2cebdc94603f2
Akf161 <akolj161@gmail.com>
2023-01-02 08:13:15 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  * Return: dest
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
@@ -16,7 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	while (i < n)
 	{
-		*(dest + i) = *(s + i);
+		*(dest + i) = *(src + i);
 		i++;
 	}
 
