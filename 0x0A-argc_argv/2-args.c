65a186ae415b052742ca35644f38b2129e9edfae
Akf161 <akolj161@gmail.com>
2023-01-06 13:33:38 -0800

    task 2, 3, 4 and 5

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments
+ * @argc: count
+ * @argv: argument
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
-int check_num(char *str);
+int chnum(char *str);
 
 #endif
